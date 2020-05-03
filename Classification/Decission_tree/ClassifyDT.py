37f47ffd48957734b7042b3b860818340e044cb7
David Pascual <paskymail@gmail.com>
2020-05-03 14:45:37 +0200

    first run

--- Classification/Decission_tree/ClassifyDT.py
+++ Classification/Decission_tree/ClassifyDT.py
@@ -2,6 +2,10 @@ def classify(features_train, labels_train):
     
     ### your code goes here--should return a trained decision tree classifer
 
+    from sklearn import tree
+
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(features_train, labels_train)
     
     
     return clf
